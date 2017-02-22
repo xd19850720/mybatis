d9ee88c5582164c3357d1cadd3d6e9b8349346b6
xuda <xd19850720@163.com>
2017-02-22 22:56:59 +0800

    add

--- src/com/kaikeba/mapper/UserMapper.java
+++ src/com/kaikeba/mapper/UserMapper.java
@@ -1,8 +1,19 @@
 package com.kaikeba.mapper;
 
+import java.util.List;
+
 import com.kaikeba.bean.User;
 
 public interface UserMapper {
 
 	public User findUserById(int id) throws Exception;
+	
+	public List<User> findUserByName(String username) throws Exception;
+	
+	public void deleteUserById(int id) throws Exception;
+	
+	public void insertUser(User user) throws Exception;
+	
+	public void updateUserById(User user) throws Exception;
+	public int findUserCount(String user) throws Exception;
 }
--- test/daotest/UserDaoTest.java
+++ test/daotest/UserDaoTest.java
@@ -2,6 +2,8 @@ package daotest;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
 
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -34,7 +36,63 @@ private SqlSessionFactory sqlSessionFactory;
 	public void testFindUserById() throws Exception {
 		SqlSession session = sqlSessionFactory.openSession();
 		UserMapper userMapper = session.getMapper(UserMapper.class);
-		User user = userMapper.findUserById(2);
+		User user = userMapper.findUserById(1);
 		System.out.println(user.getUsername());
+	}
+	
+	@Test
+	public void testFindUsersByName() throws Exception {
+		SqlSession session = sqlSessionFactory.openSession();
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+		List<User> users = userMapper.findUserByName("张三");
+		System.out.println(users.get(0).getUsername());
+	}
+	
+	@Test
+	public void testInsertUser() throws Exception {
+		SqlSession session = sqlSessionFactory.openSession();
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+		
+		User user = new User();
+		user.setId(2);
+		user.setAddress("石景山");
+		user.setBirthday(new Date());
+		user.setSex("2");
+		user.setUsername("xuda");
+		
+		userMapper.insertUser(user);
+		session.commit();		
+	}
+	
+	
+	@Test
+	public void testUpdateUserById() throws Exception {
+		SqlSession session = sqlSessionFactory.openSession();
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+		
+		User user = new User();
+		user.setId(2);
+		user.setAddress("石景山");
+		user.setBirthday(new Date());
+		user.setSex("2");
+		user.setUsername("xuda1");
+		
+		userMapper.updateUserById(user);
+		session.commit();		
+	}
+	
+	@Test
+	public void testDeleteUser() throws Exception {
+		SqlSession session = sqlSessionFactory.openSession();
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+		userMapper.deleteUserById(2);
+		session.commit();		
+	}
+	@Test
+	public void testFindUserCount() throws Exception {
+		SqlSession session = sqlSessionFactory.openSession();
+		UserMapper userMapper = session.getMapper(UserMapper.class);
+		int count = userMapper.findUserCount("小明");
+		System.out.println(count);	
 	}
 }
